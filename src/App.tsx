a4388f12138faa927a7898584310635cf2980dfa
Nisala Dulanaka <dulanakanisala654@gmail.com>
2024-07-27 10:54:57 +0530

    Added Components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <>
       <h2>Hellow This is Classroom Management System 2024</h2>
+      <Sidebar />
     </>
   )
 }
